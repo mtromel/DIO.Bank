682d5a879510c9cc095a0ec142257b42cc4d6c74
Marcos Tromel <marcos.tromel@gmail.com>
2021-03-11 09:49:36 -0300

    primeiro commit do projeto

---
+++ DIO.Bank/Classes/Conta.cs
@@ -0,0 +1,67 @@
+using System;
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+
+        private double Saldo { get; set; }
+
+        private double Credito { get; set; }
+
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+        public bool Sacar(double valorSaque)
+        {
+            // Validação de saldo suficiente
+            if (this.Saldo - valorSaque < (this.Credito *-1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+            // essa linha acima é a mesma coisa que a linha abaixo
+            // this.Saldo = this.Saldo - valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            // essa forma usada acima é uma formatação composta da string, abaixo a documentação:
+            // https://docs.microsoft.com/pt-br/dotnet/standard/base-types/composite-formatting
+
+            return true;
+        }
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+            // essa linha acima é a mesma coisa que a linha abaixo
+            // this.Saldo = this.Saldo + valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta {0} é {1}", this.Nome, this.Saldo);
+        }
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            // sobrescrevendo a string ToString da classe mãe para retornar conforme abaixo
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Crédito " + this.Credito;
+            return retorno;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# App do DIO Bank
+
+# 			:bank:
+
+
+
+
+
+## Aplicação de transferência bancária com .NET
+
+Esta pasta contém um algoritmo simples de transferência bancária para exercer o pensamento orientado a objetos, o principal paradigma de programação utilizado no mercado. Nesse projeto vemos como pensar orientado a objetos, como modelar o dominio e como utilizar enums
+
+
+
+
+
+
+
